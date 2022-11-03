4867688dd479bb38d3820d2726b0e8510b1eae32
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-11-03 13:48:39 +0800

    initial

--- src/util.py
+++ src/util.py
@@ -16,11 +16,12 @@ import torch.nn.functional as F
 import cv2
 import math
 from scipy import interpolate
+import copy
 
 def load_mean_theta():
     mean = np.zeros(args.total_theta_count, dtype = np.float)
 
-    mean_values = h5py.File(args.smpl_mean_theta_path)
+    mean_values = copy.deepcopy(h5py.File(args.smpl_mean_theta_path))
     mean_pose = mean_values['pose']
     mean_pose[:3] = 0
     mean_shape = mean_values['shape']
