2626a3fba59fbacea17b0d950b6a3d0f50010005
SmallPigPeppa <905328134@qq.com>
2022-12-06 15:45:07 +0800

    add gta loader

--- src/gta2hdf5_wiith_mover_smplx_to_smpl_14.py
+++ src/gta2hdf5_wiith_mover_smplx_to_smpl_14.py
@@ -1,171 +1,149 @@
-'''
-    file:   hum36m_dataloader.py
-
-    author: zhangxiong(1025679612@qq.com)
-    date:   2018_05_09
-    purpose:  load hum3.6m data
-'''
-
-import sys
-from torch.utils.data import Dataset, DataLoader
-import os
-import glob
-import numpy as np
-import random
-import cv2
-import json
 import h5py
-import torch
+import numpy
+import numpy as np
+from kpts_mapping.gta import GTA_ORIGINAL_NAMES, GTA_KEYPOINTS
+from kpts_mapping.gta_im import GTA_IM_NPZ_KEYPOINTS, GTA_IM_PKL_KEYPOINTS
+from kpts_mapping.smplx import SMPLX_KEYPOINTS
+from kpts_mapping.smpl import SMPL_KEYPOINTS
 import pickle
+import os
+import cv2
+from scipy.spatial.transform import Rotation as R
 
-sys.path.append('./src')
-from util import calc_aabb, cut_image, flip_image, draw_lsp_14kp__bone, rectangle_intersect, \
-    get_rectangle_intersect_ratio, convert_image_by_pixformat_normalize, reflect_pose, reflect_lsp_kp
-from config import args
-from timer import Clock
-
-
-class gta_dataloader(Dataset):
-    def __init__(self, data_set_path, use_crop, scale_range, use_flip, min_pts_required, pix_format='NHWC',
-                 normalize=False, flip_prob=0.3):
-        self.data_folder = data_set_path
-        self.use_crop = use_crop
-        self.scale_range = scale_range
-        self.use_flip = use_flip
-        self.flip_prob = flip_prob
-        self.min_pts_required = min_pts_required
-        self.pix_format = pix_format
-        self.normalize = normalize
-        self._load_data_set()
-
-    def _load_data_set(self):
-
-        clk = Clock()
-
-        self.images = []
-        self.kp2ds = []
-        self.boxs = []
-        self.kp3ds = []
-        self.shapes = []
-        self.poses = []
-        self.masks=[]
-
-        print('start loading gta-im data.')
-        anno_file_path = os.path.join(self.data_folder, 'annot.h5')
+# step0:
+# merge info_frames.pickle info_frames.npz
+# trans world to camera
 
 
-        with h5py.File(anno_file_path) as fp:
-            total_kp2d = np.array(fp['gt2d'])
-            total_kp3d = np.array(fp['gt3d'])
-            total_shap = np.array(fp['shape'])
-            total_pose = np.array(fp['pose'])
-            print(total_pose.shape)
+data_root = './'
+rec_idx = '2020-06-11-10-06-48'
+info_pkl = pickle.load(open(os.path.join(data_root, rec_idx, 'info_frames.pickle'), 'rb'))
+info_npz = np.load(open(os.path.join(data_root, rec_idx, 'info_frames.npz'), 'rb'))
+kpts_npz = np.array(info_npz['joints_3d_world'])
+kpts_pkl = np.array([i['kpvalue'] for i in info_pkl]).reshape(kpts_npz.shape[0], -1, kpts_npz.shape[2])
+kpts_pkl_names = [i['kpname'] for i in info_pkl]
+world2cam = np.array(info_npz['world2cam_trans'])
+kpts_world = np.concatenate((kpts_npz, kpts_pkl), axis=1)
+kpts_valid = np.zeros([len(kpts_world), len(kpts_world[0]), 1])
+print(
+    f'loading kpts...\nkpts_npz.shape, kpts_pkl.shape, kpts_world.shape: {kpts_npz.shape}, {kpts_pkl.shape}, {kpts_world.shape}')
+kpts_camera = []
+for i in range(len(kpts_world[0])):
+    if sum(kpts_world[0, i, :]) != 0:
+        kpts_valid[:, i] = 1
+for i in range(len(kpts_world)):
+    r_i = world2cam[i][:3, :3].T
+    t_i = world2cam[i][3, :3]
+    # cam_point_i = r_i * kpts_world[i] + t_i
+    cam_point_i = [np.matmul(r_i, kpt) + t_i for kpt in kpts_world[i]]
+    kpts_camera.append(cam_point_i)
+kpts_camera = np.concatenate((kpts_camera, kpts_valid), axis=-1)
 
-            assert len(total_kp2d) == len(total_kp3d) and \
-                   len(total_kp2d) == len(total_shap) and len(total_kp2d) == len(total_pose)
+# step1
+# gta_im to gta
+kpts_gta_im = numpy.array(kpts_camera)
+kpts_gta = numpy.zeros(shape=(len(kpts_gta_im), len(GTA_KEYPOINTS), 4))
+gta_im_names = GTA_IM_NPZ_KEYPOINTS + GTA_IM_PKL_KEYPOINTS
+mapping_list = []
 
-            l = len(total_kp2d)
+for i in range(len(kpts_gta)):
+    mapping_list_i = []
+    gta_im_names = GTA_IM_NPZ_KEYPOINTS + kpts_pkl_names[i]
+    for kpt_name in GTA_ORIGINAL_NAMES:
+        if kpt_name not in gta_im_names:
+            mapping_list_i.append(-1)
+        else:
+            mapping_list_i.append(gta_im_names.index(kpt_name))
+    mapping_list.append(mapping_list_i)
 
-            def _collect_valid_pts(pts):
-                r = []
-                for pt in pts:
-                    if pt[2] != 0:
-                        r.append(pt)
-                return r
+for i in range(len(kpts_gta)):
+    for j in range(len(kpts_gta[0])):
+        if mapping_list[i][j] != -1:
+            kpts_gta[i][j] = kpts_gta_im[i][mapping_list[i][j]]
+# average for nose
+for i in range(len(kpts_gta)):
+    kpts_gta[i][-1] = np.average(kpts_gta[i][45:51], axis=0)
 
-            for index in range(l):
-                kp2d = total_kp2d[index].reshape((-1, 3))
-                if np.sum(kp2d[:, 2]) < self.min_pts_required:
-                    continue
+# step2
+# gta to smpl
+mapping_list2 = []
+valid_len = 0
+for kpt_name in SMPL_KEYPOINTS:
+    if kpt_name not in GTA_KEYPOINTS:
+        mapping_list2.append(-1)
+    else:
+        mapping_list2.append(GTA_KEYPOINTS.index(kpt_name))
+        valid_len += 1
+print(f'tansform to smpl joints, num kpts: {len(mapping_list2)}, valid kpts:{valid_len}')
+# print(mapping_list2)
+kpts_smpl = numpy.zeros(shape=(len(kpts_gta), len(SMPL_KEYPOINTS), 4))
+for i in range(len(kpts_smpl)):
+    for j in range(len(kpts_smpl[0])):
+        if mapping_list2[j] != -1:
+            kpts_smpl[i][j] = kpts_gta[i][mapping_list2[j]]
 
-                lt, rb, v = calc_aabb(_collect_valid_pts(kp2d))
-                self.kp2ds.append(np.array(kp2d.copy(), dtype=float))
-                self.boxs.append((lt, rb))
-                self.kp3ds.append(total_kp3d[index].copy().reshape(-1, 3))
-                self.shapes.append(total_shap[index].copy())
-                self.poses.append(total_pose[index].copy())
+# step3: save smpl as hdf5
+h5f = h5py.File(os.path.join(data_root, rec_idx, 'annot.h5'), 'w')
+gt3d = kpts_smpl
+print('gt3d.shape',gt3d.shape)
+gt2d = []
+rvec = np.zeros([3, 1], dtype=float)
+tvec = np.zeros([3, 1], dtype=float)
+dist_coeffs = np.zeros([4, 1], dtype=float)
+for i in range(len(gt3d)):
+    camera_matrix_i = info_npz['intrinsics'][i]
+    gt3d_i = np.array(gt3d[i, :, :3], dtype=float)
+    gt2d_i, _ = cv2.projectPoints(gt3d_i, rvec, tvec, camera_matrix_i, dist_coeffs)
+    gt2d.append(gt2d_i)
+gt2d = np.squeeze(np.array(gt2d, dtype=float))
+gt3d = np.array(gt3d, dtype=float)
+valid_kpts = gt3d[:, :, -1].reshape(len(gt3d), -1, 1)
+print('gt2d.shape, valid_kpts.shape',gt2d.shape, valid_kpts.shape)
+gt2d = np.concatenate((gt2d, valid_kpts), axis=-1)
 
-                img_i = os.path.join(self.data_folder, '{:05d}'.format(index) + '.jpg')
-                mask_i = os.path.join(self.data_folder, '{:05d}'.format(index) + '_id.png')
-                assert os.path.exists(img_i) and os.path.exists(mask_i)
-                self.images.append(img_i)
-                self.masks.append(mask_i)
-        print('finished load gta-im data, total {} samples'.format(len(self.kp3ds)))
-        clk.stop()
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, index):
-        image_path = self.images[index]
-        kps = self.kp2ds[index].copy()
-        box = self.boxs[index]
-        kp_3d = self.kp3ds[index].copy()
-
-        scale = np.random.rand(4) * (self.scale_range[1] - self.scale_range[0]) + self.scale_range[0]
-        image, kps = cut_image(image_path, kps, scale, box[0], box[1])
-
-        ratio = 1.0 * args.crop_size / image.shape[0]
-        kps[:, :2] *= ratio
-        dst_image = cv2.resize(image, (args.crop_size, args.crop_size), interpolation=cv2.INTER_CUBIC)
-
-        trival, shape, pose = np.zeros(3), self.shapes[index], self.poses[index]
-
-        if self.use_flip and random.random() <= self.flip_prob:
-            dst_image, kps = flip_image(dst_image, kps)
-            pose = reflect_pose(pose)
-            kp_3d = reflect_lsp_kp(kp_3d)
-
-        # normalize kp to [-1, 1]
-        ratio = 1.0 / args.crop_size
-        kps[:, :2] = 2.0 * kps[:, :2] * ratio - 1.0
-
-        theta = np.concatenate((trival, pose, shape), axis=0)
-
-        return {
-            'image': torch.from_numpy(
-                convert_image_by_pixformat_normalize(dst_image, self.pix_format, self.normalize)).float(),
-            'kp_2d': torch.from_numpy(kps).float(),
-            'kp_3d': torch.from_numpy(kp_3d).float(),
-            'theta': torch.from_numpy(theta).float(),
-            'image_name': self.images[index],
-            'w_smpl': 1.0,
-            'w_3d': 1.0,
-            'data_set': 'hum3.6m'
-        }
+# h5f.create_dataset('gt2d', data=gt2d)
+# h5f.create_dataset('gt3d', data=gt3d)
+# h5f.create_dataset('gt2d', data=gt2d[:, :, :3])
+# h5f.create_dataset('gt3d', data=gt3d[:, :, :3])
+h5f.create_dataset('gt2d', data=gt2d[:, :14, :3])
+h5f.create_dataset('gt3d', data=gt3d[:, :14, :3])
 
 
-# if __name__ == '__main__':
-# #     h36m = hum36m_dataloader('E:/HMR/data/human3.6m', True, [1.1, 2.0], True, 5, flip_prob=1)
-#     data_dir='C:/Users/90532/Desktop/Datasets/HMR/2020-06-11-10-06-48'
-#     gta_loader=gta_dataloader(data_dir, True, [1.1, 2.0], True, 5, flip_prob=1)
-#     l = len(gta_loader)
-#     for _ in range(l):
-#         r = gta_loader.__getitem__(_)
-#         print(r)
-#         break
+# step3: get smplx mesh pose and shape
+smpl_pkl_folder = 'C:\\Users\\90532\\Desktop\\code\\smplx\\output'
+smpl_poses_matrix = []
+smpl_betas = []
+for filename in os.listdir(smpl_pkl_folder):
+    if filename.endswith('.pkl'):
+        file_i = open(os.path.join(smpl_pkl_folder, filename), "rb")
+        smpl_parm_i = pickle.load(file_i)
+        pose_matrix_i = smpl_parm_i['full_pose'].cpu().detach().numpy().reshape(-1, 3, 3)
+        beta_i = smpl_parm_i['betas'].cpu().detach().numpy().reshape(-1)
+        smpl_poses_matrix.append(pose_matrix_i)
+        smpl_betas.append(beta_i)
+        file_i.close()
 
-if __name__=='__main__':
-    data_set_path='C:/Users/90532/Desktop/Datasets/HMR/2020-06-11-10-06-48'
-    pix_format = 'NCHW'
-    normalize = True
-    flip_prob = 0.5
-    use_flip = False
-    gta_loader = gta_dataloader(
-        data_set_path=data_set_path,
-        use_crop=True,
-        scale_range=[1.1, 2.0],
-        use_flip=True,
-        min_pts_required=5,
-        pix_format=pix_format,
-        normalize=normalize,
-        flip_prob=flip_prob
-    )
-    # for idx,i in enumerate(iter(gta_loader)):
-    #     if i['kp_2d'].shape!=i['kp_3d'].shape:
-    #         print('frame{idx}'.format(idx=idx))
-    #         print(i['kp_2d'].shape,i['kp_3d'].shape)
-    for idx,i in enumerate(iter(gta_loader)):
-        if len(i['kp_2d'])!=23:
-            print('frame{idx}'.format(idx=idx))
-            print(i['kp_2d'].shape,i['kp_3d'].shape)
+smpl_poses_matrix = np.array(smpl_poses_matrix).reshape(-1, 3, 3)
+rot_matrix = R.from_matrix(smpl_poses_matrix)
+smpl_poses_rotvec = rot_matrix.as_rotvec().reshape(-1, 24, 3)
+
+smpl_betas = np.array(smpl_betas)
+# smpl_shapes = np.average(smpl_betas, axis=0)
+# h5f_shapes = np.repeat(smpl_shapes, len(kpts_smplx), axis=0)
+
+h5f_shapes = smpl_betas.reshape(-1,10)
+h5f_poses = smpl_poses_rotvec.reshape(-1,72)
+print('h5f_poses.shape,h5f_shapes.shape',h5f_poses.shape,h5f_shapes.shape)
+
+h5f.create_dataset('shape', data=h5f_shapes)
+h5f.create_dataset('pose', data=h5f_poses)
+h5f.close()
+
+
+with h5py.File(os.path.join(data_root, rec_idx, 'annot.h5'), "r+") as f:
+    print("Keys: %s" % f.keys())
+    print("np.array(f['gt2d']).shape",np.array(f['gt2d']).shape)
+    print("np.array(f['gt3d']).shape",np.array(f['gt3d']).shape)
+    print("np.array(f['pose']).shape,np.array(f['shape']).shape",np.array(f['pose']).shape,np.array(f['shape']).shape)
+    # print(np.array(f['shape']).shape)
+    # print(str(np.array(f['pose'][0])).replace('  ', ',').replace(' ', ','))
